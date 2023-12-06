305cdd3f4d2021138890e5abd0848aba9b8364bf
WaleedRazaShoaib <146237300+WaleedRazaShoaib@users.noreply.github.com>
2023-12-06 12:59:48 +0500

    Update app.js

--- app.js
+++ app.js
@@ -1,14 +1,12 @@
 var input =  document.getElementById("input");
-var input2 =  document.getElementById("input2");
 function values(num){
-  
   console.log(num)
   input.value += num;
-
 }
 input.style.fontSize = "30px";
 function equal(){
    console.log(input.value) 
   input.value = eval(input.value)
-  Math.round(input.value)  
+  Math.round(input.value) 
+
 }
