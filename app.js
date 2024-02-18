1dde9a2c2142a967d992369c7c7f07d7f574ab92
WaleedRazaShoaib <146237300+WaleedRazaShoaib@users.noreply.github.com>
2024-02-18 23:58:37 +0500

    Update app.js

--- app.js
+++ app.js
@@ -53,3 +53,20 @@ let total = 0;
 for (let i = myArr.length - 1; i >= 0; i--) {
   total += myArr[i];
 }
+
+// program to count down numbers to 1
+function countDown(number) {
+
+  // display the number
+  console.log(number);
+
+  // decrease the number value
+  const newNumber = number - 1;
+
+  // base case
+  if (newNumber > 0) {
+      countDown(newNumber);
+  }
+}
+
+countDown(4);
