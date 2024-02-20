263afffe70936f7cf5a17159a0b05535f5ac4821
WaleedRazaShoaib <146237300+WaleedRazaShoaib@users.noreply.github.com>
2024-02-20 23:54:00 +0500

    Update app.js

--- app.js
+++ app.js
@@ -107,4 +107,11 @@ function createGitHubIssue(title, description) {
     icon: 'success',
     confirmButtonText: 'Okay'
   });
-}
+};
+
+// Assign the value 5 to x
+let x = 5;
+// Assign the value 2 to y
+let y = 2;
+// Assign the value x + y to z:
+let z = x + y;
