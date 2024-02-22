6e2ef60b5f7f0c3ef30a380f7e173fc61c64d3c8
WaleedRazaShoaib <146237300+WaleedRazaShoaib@users.noreply.github.com>
2024-02-22 23:47:37 +0500

    Update app.js

--- app.js
+++ app.js
@@ -126,3 +126,23 @@ function createGitHubIssue(title, description) {
     confirmButtonText: 'Okay'
   });
 };
+
+
+const number1 = parseFloat(prompt ('Enter the first number: '));  
+const number2 = parseFloat(prompt ('Enter the second number: '));  
+  
+let result; // declaration of the variable.  
+  
+// use if, elseif and else keyword to define the calculator condition in JavaScript.  
+if (operator == '+') { // use + (addition) operator to add two numbers  
+    result = number1 + number2;  
+}  
+else if (operator == '-') { // use -  (subtraction) operator to subtract two numbers  
+    result = number1 - number2;  
+}  
+else if (operator == '*') { // use * (multiplication) operator to multiply two numbers  
+    result = number1 * number2;  
+}  
+else {  
+    result = number1 / number2; // use / (division) operator to divide two numbers  
+}  
